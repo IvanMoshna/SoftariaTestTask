54acd9791892b8886831b7807dca03f4c5d8a0e7
Ivan Moshna <ittifock@gmail.com>
2021-01-26 17:16:57 +0700

    create fillMapWithHtmlCode method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HashMap<String, String> todayMap = new HashMap<>();
+        HashMap<String, String> yesterdayMap = new HashMap<>();
+
+        ArrayList<String> urlList = new ArrayList<>();
+        urlList.add("http://www.google.com");
+        urlList.add("https://yandex.ru");
+        urlList.add("https://vk.com");
+        urlList.add("https://hh.ru");
+
+        fillMapWithHtmlCode(urlList, yesterdayMap);
+    }
+
+    //заполняем мапу из списка URLов
+    public static void fillMapWithHtmlCode(ArrayList<String> urlList, HashMap<String, String> map) {
+        for (String url: urlList) {
+            String content = null;
+            URLConnection connection = null;
+            try {
+                connection =  new URL(url).openConnection();
+                Scanner scanner = new Scanner(connection.getInputStream());
+                scanner.useDelimiter("\\Z");
+                content = scanner.next();
+                scanner.close();
+            } catch ( Exception ex ) {
+                ex.printStackTrace();
+            }
+            map.put(url, content);
+            //System.out.println(content);
+        }
     }
 }
